1d4be0ef817be3a07147f4d46f4864db40129fd0
David Banks <amoebae@gmail.com>
2019-07-17 16:01:16 +0100

    initial import

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # pop-to-container
+
+## Install prerequisites
+
+Please use Fabric v2.4.0.  To install, do: `pip3 install fabric==2.4.0`
+
+## How to use
+
+python3 pop-to-container.py PROFILE
+
+Where PROFILE is `factchecking`.
---
+++ pop-to-container.py
@@ -0,0 +1,54 @@
+import fabric
+import time
+import getpass
+import sys
+
+PROFILES = {
+    'factchecking': {
+        'container_ip': '10.179.127.25',
+        'user': 'np278'
+    }
+}
+
+
+if len(sys.argv) < 2:
+    raise Exception('use a profile name as the first argument')
+
+profile_name = sys.argv[1]
+profile = PROFILES[profile_name]
+
+user = profile['user']
+
+print("Loaded profile for user {}.".format(user))
+
+# Establish contact with perimeter host
+print("I will now prompt you for your password for unix.uscs.susx.ac.uk.")
+password = getpass.getpass()
+options = {
+    'password': password
+}
+c = fabric.Connection('unix.uscs.susx.ac.uk', user=user, connect_kwargs=options)
+
+local_port = 49152
+remote_host = 'shl1.inf.susx.ac.uk'
+remote_port = 22
+
+with c.forward_local(
+        local_port,
+        remote_port=remote_port,
+        remote_host=remote_host,
+        local_host='localhost'
+): 
+    print("Set up tunnel successfully.")
+
+    # Note that user spec is *required* here because it may differ.  Note that we
+    # assume you use the same pass for both hosts.  May not be valid assumption.
+    c2 = fabric.Connection('localhost', user=user, port=local_port, connect_kwargs=options)
+
+    print("Now use your root password for the container.")
+    container_ssh_command = "ssh root@{}".format(profile['container_ip'])
+    c2.run(container_ssh_command, pty=True)
+
+    print("SSH session to container terminated.")
+
+print("Tunnel has been stopped.")
